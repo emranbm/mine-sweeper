d284b4db37da81720ff423d876ce14f900350c28
Emran <emran.bm@gmail.com>
2016-11-28 21:30:54 +0330

    base window

--- minesweeper.js
+++ minesweeper.js
@@ -1,12 +1,88 @@
 // Test Funcs
 // See Inspect Element's Console Log Output
 
-getGameXML();
+// getGameXML();
+//
+// getNewGame(`
+//     <request>
+//     <rows>3</rows>
+//     <cols>3</cols>
+//     <mines>3</mines>
+//     </request>
+// `);
 
-getNewGame(`
-    <request>
-    <rows>3</rows>
-    <cols>3</cols>
-    <mines>3</mines>
-    </request>
-`);
+function main() {
+    createElements();
+}
+
+/**
+ * Creates html elements of the page
+ */
+function createElements() {
+    /**
+     * A helper function to create an element in a single line.
+     * @param tagName
+     * @param className
+     * @param id
+     * @returns {Element}
+     */
+    function newElement(tagName, className, id) {
+        let element = document.createElement(tagName);
+        if (className)
+            element.className = className;
+        if (id)
+            element.id = id;
+
+        return element;
+    }
+
+    /**
+     * Creates a div element having the given class and id.
+     * @param className
+     * @param id
+     * @returns {Element}
+     */
+    function newDiv(className, id) {
+        return newElement('div', className, id);
+    }
+
+    //Alert modal div
+    let alertModalDiv = newDiv('modal', 'alert-modal');
+    document.body.appendChild(alertModalDiv);
+
+    let alertChildDiv = newDiv('modal-content');
+    alertModalDiv.appendChild(alertChildDiv);
+
+    let name = newElement('input', 'field', 'name');
+    name.placeholder = 'Enter your name';
+    alertChildDiv.appendChild(name);
+
+    let okBtn = newElement('button');
+    okBtn.innerHTML = 'OK';
+    alertChildDiv.appendChild(okBtn);
+    //Alert modal div - END
+
+    let windowDiv = newDiv('window');
+    document.body.appendChild(windowDiv);
+
+    //Title bar div
+    let titleBarDiv = newDiv('title-bar');
+    windowDiv.appendChild(titleBarDiv);
+
+    let gameTitleSpn = newElement('span', '', 'game-title');
+    gameTitleSpn.innerHTML = 'Minesweeper Online - Beginner!';
+    titleBarDiv.appendChild(gameTitleSpn);
+
+    let titleChildDiv = newDiv();
+    titleBarDiv.appendChild(titleChildDiv);
+
+    let minimizeBtnSpn = newElement('span', 'btn', 'btn-minimize');
+    titleChildDiv.appendChild(minimizeBtnSpn);
+
+    let closeBtnSpn = newElement('span', 'btn', 'btn-close');
+    titleChildDiv.appendChild(closeBtnSpn);
+    //Title bar div - END
+}
+
+main();
+
